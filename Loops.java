5b67eebd450aed5427c9fa92887fb2738c17664c
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2025-11-21 10:03:39 +0530

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,24 @@
+public class Loops{
+    public static void main(String args[]){
+
+        // for loop
+        for(int i = 1; i<= 10; i++){
+           System.out.println(i);
+        }
+
+
+         // while loop
+         int i = 1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+       }
+
+       // do while loop
+       i = 1;
+       do{
+        System.out.println(i);
+        i = i+1;
+       }while(i<11);
+    }
+}
