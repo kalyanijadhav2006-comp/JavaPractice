211c33862382b339b2908f718e3d8ed9ccd60c94
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2026-01-09 05:10:46 +0530

    Add files via upload

---
+++ NumberPrimeOrNot.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class NumberPrimeOrNot{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        boolean isprime = true;
+        for(int i=2; i<=n/2; i++){
+            if(n%i == 0){
+                isprime = false;
+                break;
+            }
+        }
+        if(isprime){
+            if(n==1){
+                System.out.println("This is neither prime nor composite.");
+            }else{
+                System.out.println("this is prime number");
+            }
+            
+        }else{
+            System.out.println("This is not prime number");
+        }
+    }
+}
