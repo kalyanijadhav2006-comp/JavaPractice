cece6e33a9914c1942f59b396d5ac25cfa0cedb7
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2026-01-12 19:16:29 +0530

    Add files via upload

---
+++ OneofGreaterNo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OneofGreaterNo{
+    static int greaternumber(int a, int b){
+        if(a>b){
+            return a;
+        }else{
+            return b;
+    }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(greaternumber(a,b));
+    }
+}
