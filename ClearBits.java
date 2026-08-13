cb6cd1662da716483fbc1963f305c5e46142e9c8
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2026-08-13 18:23:29 +0530

    Add files via upload

---
+++ ClearBits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ClearBits{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+int pos = sc.nextInt();
+int bitmask = 1 << pos;
+
+    int newbitmask = ~(bitmask);
+
+    int newNumber = newbitmask & n;
+    System.out.println(newNumber);
+}
+
+
+}
