62205450e8410476194fee7017bbb132dc9f6866
kalyanijadhav2006-comp <kalyanijadhav2006@gmail.com>
2026-08-16 21:24:21 +0530

    Add files via upload

---
+++ SelectionSorting.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class SelectionSorting{
+
+    public static void sort(int n, int arr[]){
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+        //input 
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        //sorting
+        for(int i=0; i<arr.length-1; i++){
+            int smallest = arr[i];
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[smallest] > arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        sort(n, arr);
+    }
+}
